dec7cd4ae176262f6c333574cd490fb048e9a986
sharif <sharif.bashoma@gmail.com>
2021-03-19 17:55:05 +0330

    normalize app Type column

--- Recommendation/appRecommendation.py
+++ Recommendation/appRecommendation.py
@@ -1,12 +1,9 @@
+from numpy import NaN, nan
 from pandas.core.series import Series
-# from .recommendationSystemMethods import GetRelation
+from recommendationSystemMethods import *
 from scipy import stats
 from pandas import read_csv
 import pandas as pd
-
-def GetRelation(x, y):
-    slope, intercept , r , p , std_err = stats.linregress(x,y)
-    return r
 
 df = read_csv("data/googleplaystore.csv", float_precision='round_trip')
 
@@ -28,6 +25,22 @@ for i in range(len(rating)):
         rating[i] = 0
         continue
 
+appType = df["Type"]
+# print(type(appType))
+# x = findDiffrentValue(df,"Type")
+# for i in x:
+#     print(type(i))
+for i in range(len(appType)):
+    if type(appType[i]) is float:
+        appType[i] = 0
+    elif appType[i] == '0' or 'Free':
+         appType[i] = 0
+    elif  appType[i] == 'Paid':
+         appType[i] = 1
+
+price = df["Price"]
+x = findDiffrentValue(df,"Price")
+print(x)
 print(GetRelation(download, rating))
 
 

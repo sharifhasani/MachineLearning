640299518b97e08df74caf2ef1606b671cb44757
sharif <sharif.bashoma@gmail.com>
2021-03-19 18:38:25 +0330

    test sample for binary classification

---
+++ classification/binaryClassification.py
@@ -0,0 +1,26 @@
+# example of binary classification task
+from numpy import where
+from collections import Counter
+from sklearn.datasets import make_blobs
+from matplotlib import pyplot
+# define dataset
+X, y = make_blobs(n_samples=1000, centers=5, random_state=1)
+# for i in X:
+#     if i in c:
+#         print(i)
+# print(c)
+# exit()
+# summarize dataset shape
+print(X.shape, y.shape)
+# summarize observations by class label
+counter = Counter(y)
+print(counter)
+# summarize first few examples
+for i in range(10):
+	print(X[i], y[i])
+# plot the dataset and color the by class label
+for label, _ in counter.items():
+	row_ix = where(y == label)[0]
+	pyplot.scatter(X[row_ix, 0], X[row_ix, 1], label=str(label))
+pyplot.legend()
+pyplot.show()
